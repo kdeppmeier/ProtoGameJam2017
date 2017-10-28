595757ea489f332d1299be46f2fd0e6d9ef81482
Blake Wakasa <bwakasa@uci.edu>
2017-10-27 21:08:36 -0700

    Added PlayerMovementScript

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed;
+    private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-transform.right * speed);
+        }
+        else if (Input.GetKey("d"))
+        {
+            rb.AddForce(transform.right * speed);
+        }   
+    }
+}
