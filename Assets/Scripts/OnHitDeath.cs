0c604cfc7c24b3ffb19b3f4532c169b1bf5a6f64
Blake Wakasa <bwakasa@uci.edu>
2017-10-28 12:16:58 -0700

    Add 'Criminal' to prefabs and  change my scene

--- Assets/Scripts/OnHitDeath.cs
+++ Assets/Scripts/OnHitDeath.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class OnHitDeath : MonoBehaviour {
+    public string scene_name = "GameOver";
     void OnTriggerEnter2D(Collider2D other)
     {
-        SceneManager.LoadScene("GameOver");
+        SceneManager.LoadScene(scene_name);
     }
     // Use this for initialization
     void Start () {
