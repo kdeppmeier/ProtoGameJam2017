2acf3dff7fecb2a8e2bd27b2b226d72eb27365fc
Ben M <meib1@uci.edu>
2017-10-28 01:53:26 -0700

    Modify Movement Code
    Able to press two buttons at once and remove double jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     public float speed;
-    public float jumpSpeed;
+    public float jumpHeight;
     public float groundDistance;
 
     private Rigidbody2D rb;
@@ -20,21 +20,28 @@ public class PlayerMovement : MonoBehaviour {
         if (Input.GetKey("a"))
         {
             //rb.AddForce(-transform.right * speed);ight
-            rb.MovePosition(rb.position + speed * Time.deltaTime * Vector2.left);
+            //rb.MovePosition(rb.position + speed * Time.deltaTime * Vector2.left);
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
         }
         else if (Input.GetKey("d"))
         {
             //rb.AddForce(transform.right * speed);
-            rb.MovePosition(rb.position + speed * Time.deltaTime * Vector2.right);
+            //rb.MovePosition(rb.position + speed * Time.deltaTime * Vector2.right);
+            rb.velocity = new Vector2(speed, rb.velocity.y);
         }
-        else if (Input.GetKey("w"))
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+        if (Input.GetKeyDown("w"))
         {
             RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundDistance, groundLayer);
 
             if (hit.collider != null)
             {
                 //rb.MovePosition(rb.position + speed * Time.deltaTime * Vector2.up);
-                rb.AddForce(Vector2.up * jumpSpeed);
+                //rb.AddForce(Vector2.up * jumpSpeed);
+                rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
             }
         }  
     }
