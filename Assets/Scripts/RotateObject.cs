2c32d2db760d42d741dec8dc849a12e07760e658
Blake Wakasa <bwakasa@uci.edu>
2017-10-28 15:33:07 -0700

    Change to Blake scene and Add RotateObject script

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb2D;
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        rb2D.MoveRotation(rb2D.rotation + speed * Time.fixedDeltaTime);
+    }
+}
